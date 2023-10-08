e3cf30e1d9b3554516be2862424de79e138c88dc
gholizadeh.m <gholizadeh.m@ronixtools.com>
2023-10-08 10:53:49 +0330

    add layout file  foodapp

---
+++ components/layout/Layout.js
@@ -0,0 +1,19 @@
+
+function Layout({children}) {
+    return (
+        <>
+        <header>
+            BotoFood
+        </header>
+        <div>
+            {children}
+        </div>
+        <footer>
+            Next.js Test BotoFood App project
+        </footer>
+        </>
+        
+    )
+}
+
+export default Layout
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div >
-      boto car
+      Boto Food
     </div>
   )
 }
