cd45793127853afd91d31ddad8fa43a82a23b01e
gholizadeh.m <gholizadeh.m@ronixtools.com>
2023-10-08 15:12:32 +0330

    add style homepage container

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -3,16 +3,17 @@ import Banner from "../modules/Banner";
 import Companies from "../modules/Companies";
 import Definition from "../modules/Definition";
 import Guide from "../modules/Guide";
+import styles from "./HomePage.module.css";
 
 function HomePage() {
     return (
-        <>
+        <div className={styles.container}>
         <Banner/>
         <Attributes />
       <Definition />
       <Companies />
       <Guide />
-        </>
+        </div>
     )
 }
 
