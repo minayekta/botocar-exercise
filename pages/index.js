de7b4bb7d2bb74308722d1b552b5bd092bb7a8d2
gholizadeh.m <gholizadeh.m@ronixtools.com>
2023-10-08 12:39:04 +0330

    add data json and server

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 
 
-export default function Home() {
+export default function Home( { data }) {
+  console.log(data)
   return (
     <div >
       Boto Food
     </div>
   )
+}
+export async function getStaticProps (){
+  const res = await fetch('http://localhost:4000/data');
+  const data = await res.json()
+  console.log(data)
+  return({
+    props:{
+      data
+    }
+  })
 }
